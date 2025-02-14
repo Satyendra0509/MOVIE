9fce1283dc091f04852b96eadb1ba79476d3f6d3
Satyendra Kumar Baghel <146946196+Satyendra0509@users.noreply.github.com>
2025-02-14 16:59:05 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "Movie-Recommender-System-Using-Machine-Learning"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "Movie-Recommender-System"
+AUTHOR_USER_NAME = "SATTU"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = ['streamlit']
 
@@ -14,13 +14,12 @@ setup(
     name=SRC_REPO,
     version="0.0.1",
     author=AUTHOR_USER_NAME,
-    description="A small package for Movie Recommender System",
+    description="A Package for Movie Recommender System",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     author_email="entbappy73@gmail.com",
     packages=[SRC_REPO],
-    license="MIT",
     python_requires=">=3.7",
     install_requires=LIST_OF_REQUIREMENTS
 )
